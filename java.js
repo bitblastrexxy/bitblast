6851ae123800159292afdc4aa125433d4ebd507b
bitblastrexxy <bot.gexxy11@gmail.com>
2024-10-13 19:08:26 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -1,16 +1,4 @@
-const toggleBalanceBtn = document.getElementById('toggleBalance');
-const balanceDisplay = document.getElementById('balance');
 
-// Replace this with the actual balance
-let actualBalance = "1000.00"; // Example balance
-
-toggleBalanceBtn.addEventListener('click', () => {
-    if (balanceDisplay.innerText === '*****') {
-        balanceDisplay.innerText = actualBalance;
-    } else {
-        balanceDisplay.innerText = '*****';
-    }
-});
 
 
 let slideIndex = 0;
